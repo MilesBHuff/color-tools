adbfc62b00bab86337aeef25263177a0bcd9e4ad
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-06-23 16:46:48 -0400

    Create new project with code from #04fd611 of https://github.com/MilesBHuff/Resume.git

---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import {createApp} from 'vue';
+import Main from './main.vue';
+createApp(Main).mount('#vue');
---
+++ src/main.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    import CalculatorPage from './pages/calculator.vue';
+</script>
+<!-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
+<template>
+    <CalculatorPage/>
+</template>
+<!-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
+<style lang="scss" id="styles-global">
+</style>
