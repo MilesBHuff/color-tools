e075f2ce8f2fbf70e77e90cabbb7e8db08cbb684
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-06-24 17:22:52 -0400

    Improved threshold scaling.

--- src/content/wcag-results.vue
+++ src/content/wcag-results.vue
@@ -13,7 +13,7 @@
         rgbToLab(props.foreground),
         rgbToLab(props.background),
     ));
-    const contrastDeviance = 7.72 / 4; // The difference between 21-scaled CIEDE2000 and WCAG for 50% RGB gray (#7f7f7f)
+    const contrastDeviance = 8.371755338567073 / 4.54; // The difference between 21-scaled CIEDE2000 and WCAG for 50% RGB gray (#7f7f7f)
 
     interface ThresholdType {
         aa: number,
