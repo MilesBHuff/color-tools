325f1ef7dfb6adf1c3a9826fb53b93d8265b6b4b
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-06-24 00:46:13 -0400

    Got the first form working.
    Thanks for the help, ChatGPT4.

--- src/pages/calculator.vue
+++ src/pages/calculator.vue
@@ -1,17 +1,54 @@
 <script setup lang="ts">
+    import {computed, ref, type Ref} from 'vue';
+    
+    const hexPattern = /^(?:[a-f0-9]{3}|[a-f0-9]{6})$/i;
+
+    const isValidHex = (value: string) => hexPattern.test(value);
+
+    const setValues = (targetRef: Ref, targetNum: Ref) => 
+        (newValue: string): void => {
+            targetRef.value = newValue;
+            if(hexPattern.test(newValue)) {
+                targetNum.value = parseInt(newValue, 16);
+            }
+        }
+
+    const foregroundRawRef = ref('000000');
+    const foregroundNumRef = ref(0x000000);
+
+    const backgroundRawRef = ref('ffffff');
+    const backgroundNumRef = ref(0xffffff);
+
+    const foreground = computed({
+        get: () => foregroundRawRef.value,
+        set: setValues(foregroundRawRef, foregroundNumRef),
+    });
+    const background = computed({
+        get: () => backgroundRawRef.value,
+        set: setValues(backgroundRawRef, backgroundNumRef),
+    });
+
+    const foregroundValid = computed(() => isValidHex(foreground.value));
+    const backgroundValid = computed(() => isValidHex(background.value));
+
+    const submitHexes = (event: Event) => {
+        if (!foregroundValid.value || !backgroundValid.value) {
+            event.preventDefault();
+        }
+    }
 </script>
 <!-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
 <template>
     <div id="calculator">
         <div id="forms">
-            <form>
+            <form @submit.prevent="submitHexes">
                 <div>
                     <label for="foreground">Foreground:</label>
-                    <input id="foreground" name="foreground" value="#000000"/>
+                    <input id="foreground" name="foreground" v-model="foreground" maxlength="6" required :class="{'invalid-input': !foregroundValid}" @input="submitHexes"/>
                 </div>
                 <div>
                     <label for="background">Background:</label>
-                    <input id="background" name="background" value="#ffffff"/>
+                    <input id="background" name="background" v-model="background" maxlength="6" required :class="{'invalid-input': !backgroundValid}" @input="submitHexes"/>
                 </div>
             </form>
             <form id="results">
@@ -37,6 +74,9 @@
 </template>
 <!-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
 <style lang="scss">
+    .invalid-input {
+        border-color: #f00 !important;
+    }
 
     #calculator {
         display: flex;
@@ -64,7 +104,7 @@
                     >input {
                         border-radius: 0.25rem;
                         font-family: monospace;
-                        
+
                         color: #000;
                         background-color: #fff;
                         border: 1px solid #7f7f7f;
