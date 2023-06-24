ad7c87804795b72a93077253f2d33770aec810ee
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-06-24 03:33:10 -0400

    Implemented RGB -> Lab

--- src/functions/rgbToLab.function.ts
+++ src/functions/rgbToLab.function.ts
@@ -1,6 +1,20 @@
 import type {LabType, RgbType} from "@/types/color-models.type";
+import "rgb-lab-conversion";
+
+type tripletType = [number, number, number];
+type iccType = 'AdobeRGB1998' | 'eciRGB v2' | 'sRGB IEC61966-2.1';
+
+declare const rgbLabConversion: {
+    rgb2Lab: (
+        rgbTriplet: tripletType,
+        iccProfileName: iccType,
+    ) => tripletType,
+};
 
 /** Convert RGB into CIELAB. */
 export const rgbToLab = (rgb: RgbType): LabType => {
-    return {l: 100, a: 0, b: 0}; //TODO
+    let rgbTriplet: tripletType = [rgb.r, rgb.b, rgb.g]
+    let iccProfileName: iccType = 'sRGB IEC61966-2.1'
+    let labTriplet = rgbLabConversion.rgb2Lab(rgbTriplet, iccProfileName);
+    return {l: labTriplet[0], a: labTriplet[1], b: labTriplet[2]};
 }
