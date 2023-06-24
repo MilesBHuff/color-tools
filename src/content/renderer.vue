037605f9d664a191ee551262a1d011902d523780
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-06-24 18:15:12 -0400

    Moved Renderer into own file

---
+++ src/content/renderer.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+    import type {RGBColor} from 'color-diff';
+
+    const props = defineProps<{
+        foreground: RGBColor,
+        background: RGBColor,
+    }>();
+</script>
+<!-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
+<template>
+    <div id="renderer" :style="{
+        'color': `rgb(${props.foreground.R}, ${props.foreground.G}, ${props.foreground.B})`,
+        'border-color': `rgb(${props.foreground.R}, ${props.foreground.G}, ${props.foreground.B})`,
+        'background-color': `rgb(${props.background.R}, ${props.background.G}, ${props.background.B})`,
+    }">
+        <span id="text-small">Lorem ipsum dolor sit amet</span>
+        <span id="text-large">consectetur adipiscing elit</span>
+    </div>
+</template>
+<!-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
+<style lang="scss" scoped>
+    #renderer {
+        border-radius: 0.25rem;
+        padding: 2rem;
+        height: min-content;
+        width: fit-content;
+
+        background-color: #fff;
+        color: #000;
+        border: 1px solid #000;
+
+        display: flex;
+        flex-direction: column;
+        gap: 1ex;
+
+        >#text-small {
+            font-size: 12pt;
+        }
+        >#text-large {
+            font-size: 18pt;
+        }
+    }
+</style>
--- src/pages/calculator.vue
+++ src/pages/calculator.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
     import HexInput from '$/content/hex-input.vue';
+    import Renderer from '$/content/renderer.vue';
     import WcagResults from '$/content/wcag-results.vue';
     import type {RGBColor} from 'color-diff';
     import {reactive} from 'vue';
@@ -22,14 +23,7 @@
         </div>
         <div id="output">
             <WcagResults :foreground="colors.foreground" :background="colors.background"/>
-            <div id="renderer" :style="{
-                'color': `rgb(${colors.foreground.R}, ${colors.foreground.G}, ${colors.foreground.B})`,
-                'border-color': `rgb(${colors.foreground.R}, ${colors.foreground.G}, ${colors.foreground.B})`,
-                'background-color': `rgb(${colors.background.R}, ${colors.background.G}, ${colors.background.B})`,
-            }">
-                <span id="text-small">Lorem ipsum dolor sit amet</span>
-                <span id="text-large">consectetur adipiscing elit</span>
-            </div>
+            <Renderer :foreground="colors.foreground" :background="colors.background"/>
         </div>
     </div>
 </template>
@@ -47,27 +41,5 @@
         display: flex;
         flex-direction: column;
         gap: 1rem;
-    }
-
-    #renderer {
-        border-radius: 0.25rem;
-        padding: 2rem;
-        height: min-content;
-        width: fit-content;
-
-        background-color: #fff;
-        color: #000;
-        border: 1px solid #000;
-
-        display: flex;
-        flex-direction: column;
-        gap: 1ex;
-
-        >#text-small {
-            font-size: 12pt;
-        }
-        >#text-large {
-            font-size: 18pt;
-        }
     }
 </style>
